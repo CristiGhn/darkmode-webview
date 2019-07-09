b2f988ac820973ec6e3f9e80dfa2d36461a06eeb
CristiGhn <christian.ghinea@gmail.com>
2019-07-09 11:31:10 +0200

    Update WebViewController.swift

--- DarkModeWebView/DarkModeWebView/WebViewController.swift
+++ DarkModeWebView/DarkModeWebView/WebViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import WebKit
 
 class WebViewController: UIViewController, WKNavigationDelegate {
-
+    
     var webview: WKWebView?
     
     override func viewDidLoad() {
@@ -18,7 +18,7 @@ class WebViewController: UIViewController, WKNavigationDelegate {
         
         self.initWebView()
     }
-
+    
     private var demoHTML : String {
         get {
             
@@ -80,33 +80,51 @@ class WebViewController: UIViewController, WKNavigationDelegate {
             return ""
         }
     }
-
+    
     private func initWebView() {
         
-            let html = self.demoHTML
-            let jsLibrary = self.darkModeLibraryJS
-            let jsOptions = self.darkModeOptionsJS
-            
-            let webConfiguration = WKWebViewConfiguration()
-            let contentController = WKUserContentController()
+        let html = self.demoHTML
+        let jsLibrary = self.darkModeLibraryJS
+        let jsOptions = self.darkModeOptionsJS
         
-            // Libray script an document start
-            let userScript = WKUserScript(source: jsLibrary, injectionTime: WKUserScriptInjectionTime.atDocumentStart, forMainFrameOnly: false)
-            contentController.addUserScript(userScript)
-
-            // Options script and document end
-            let optionsScript = WKUserScript(source: jsOptions, injectionTime: WKUserScriptInjectionTime.atDocumentEnd, forMainFrameOnly: false)
-            contentController.addUserScript(optionsScript)
+        let webConfiguration = WKWebViewConfiguration()
+        let contentController = WKUserContentController()
         
-            webConfiguration.userContentController = contentController
+        // Libray script an document start
+        let userScript = WKUserScript(source: jsLibrary, injectionTime: WKUserScriptInjectionTime.atDocumentStart, forMainFrameOnly: false)
+        contentController.addUserScript(userScript)
+        
+        // Options script and document end
+        let optionsScript = WKUserScript(source: jsOptions, injectionTime: WKUserScriptInjectionTime.atDocumentEnd, forMainFrameOnly: false)
+        contentController.addUserScript(optionsScript)
+        
+        webConfiguration.userContentController = contentController
+        
+        
+        self.webview = WKWebView(frame: CGRect.zero, configuration: webConfiguration)
+        self.webview?.navigationDelegate = self
+        self.view.addSubview(webview!)
+        
+        self.webview!.loadHTMLString(html, baseURL: nil)
+        self.webview!.fillSuperview() // after view has been added as subview
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        self.chechHTML()
+    }
+    
+    private func chechHTML() {
+        
+        let script = "document.documentElement.outerHTML.toString()"
+        
+        self.webview?.evaluateJavaScript(script, completionHandler: { (html, error) in
             
-            
-            self.webview = WKWebView(frame: CGRect.zero, configuration: webConfiguration)
-            self.webview?.navigationDelegate = self
-            self.view.addSubview(webview!)
-            
-            self.webview!.loadHTMLString(html, baseURL: nil)
-            self.webview!.fillSuperview() // after view has been added as subview
+            if html != nil {
+                print("❌: check html response", html ?? "")
+            }
+            if error != nil {
+                print("❌: check html with error", error ?? "")
+            }
+        })
     }
 }
-
